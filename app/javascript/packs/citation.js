5f50edad28c9c112c24646333519bb101d29e741
DanOlson <olson_dan@yahoo.com>
2022-11-28 20:08:56 -0500

    Absorb react-citation (#242)
    The react-citation library we've been using doesn't build in its current
    state, because one of its dev dependencies is no longer available on
    Github. By absorbing it into the project, we can leverage the existing
    Webpack config and have more flexibility over changes to the library.

--- app/javascript/packs/citation.js
+++ app/javascript/packs/citation.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ReactCitation from 'react-citation';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactCitation from '../react-citation/react-citation';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const node = document.getElementById('viewer_data')
-    const viewerNode = document.getElementById('react-citation-viewer')
-    const config = JSON.parse(node.getAttribute('citationConfig'))
-    ReactDOM.render(
-        <div className="row citation">
-          <ReactCitation items={config } />
-        </div>,
-            viewerNode.appendChild(document.createElement('div')),
-        );
-})
-
+  const node = document.getElementById('viewer_data')
+  const viewerNode = document.getElementById('react-citation-viewer')
+  const config = JSON.parse(node.getAttribute('citationConfig'))
+  ReactDOM.render(
+    <div className="row citation">
+      <ReactCitation items={config } />
+    </div>,
+    viewerNode.appendChild(document.createElement('div')),
+  );
+});
