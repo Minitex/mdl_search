db8625de6cb151f12009b07a12b9d86d286da255
Dan Olson <olson_dan@yahoo.com>
2023-03-07 19:42:22 -0500

    Don't scope search bar to current exhibit
    The search bar was searching in the context of the current exhibit, and
    that is not desired. This updates that URL to be the main, unscoped
    search URL.

--- app/components/search_bar_component.rb
+++ app/components/search_bar_component.rb
@@ -1,6 +1,10 @@
 class SearchBarComponent < Blacklight::SearchBarComponent
   attr_reader :url
 
+  def hidden_field_params
+    @params.except(:exhibit_id)
+  end
+
   def advanced_search_enabled?
     false
   end
