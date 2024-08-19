d7b05a2078949c3de016fd341e7d517d90864b30
Dan Olson <olson_dan@yahoo.com>
2024-08-18 21:17:35 -0400

    Revise tracks spec

--- spec/requests/captions_spec.rb
+++ spec/requests/captions_spec.rb
@@ -1,30 +1,37 @@
 require 'rails_helper'
 
 describe 'fetching captions for A/V items' do
-  let(:entry_id) { 'someEntryId' }
   let(:captions) { <<~VTT }
+    WEBVTT
+
     1
-    00:00:12,020 --> 00:00:14,110
-    Okay.
+    00:00:00.720 --> 00:00:05.680
+    All right. It's
+    February 19, 2019,
 
     2
-    00:00:19,220 --> 00:00:21,430
-    Right.
+    00:00:05.680 --> 00:00:07.600
+    and I'm at the Continuing Legal
 
     3
-    00:01:12,550 --> 00:01:14,885
-    You want to go to copycat?
+    00:00:07.600 --> 00:00:10.320
+    Education Center
+    in Minneapolis to
+
+    4
+    00:00:10.320 --> 00:00:12.040
+    Interview Retired
+    Minnesota Supreme
+
+    5
+    00:00:12.040 --> 00:00:13.660
+    Court Justice Allen Page
   VTT
 
-  before do
-    allow(FetchCaptionService)
-      .to receive(:fetch)
-      .with(entry_id)
-      .and_return(captions)
-  end
+  before { solr_fixtures('sll:22548') }
 
   it 'returns the captions' do
-    get "/tracks/#{entry_id}.vtt"
-    expect(response.body).to eq(captions)
+    get "/tracks/sll:22548/entry/1_fisppzr2.vtt"
+    expect(response.body).to start_with(captions)
   end
 end
