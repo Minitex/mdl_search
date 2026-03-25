31c51d1b8b5ce3d43ee8b772cf92648ce27fe42a
Dan Olson <olson_dan@yahoo.com>
2026-03-24 20:07:56 -0400

    Add additional driver config for headless chrome

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,17 @@ Sidekiq::Testing.inline!
 require 'capybara/rails'
 require 'capybara/rspec'
 Capybara.default_driver = :selenium
+Capybara.server_host = '127.0.0.1'
+
+Capybara.register_driver(:selenium_chrome_headless) do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless=new')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+end
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
